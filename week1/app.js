d5c1572b7f10682e533ea17a095ccacfd038e4f0
Alexander Pilugin <skipper-com@outlook.com>
2016-09-16 00:26:35 +0300

    week1

--- week1/app.js
+++ week1/app.js
@@ -10,17 +10,20 @@ function LunchCheckController ($scope) {
     $scope.itemEvaluate = function () {
       if (typeof $scope.itemString != 'undefined') {
         $scope.itemsArray = $scope.itemString.split(",");
-        for (var i = 0; i < $scope.itemsArray.length - 1; i++) { // <!--replace empty item, i.e., `, ,` as an item towards to the count,-->
-          if ($scope.itemsArray[i].trim() === "") {
-            $scope.itemsArray.splice(i, 1);
+        var j = 0;
+        $scope.filteredItemsArray = [];
+        for (var i = 0; i < $scope.itemsArray.length; i++) { // <!--replace empty item, i.e., `, ,` as an item towards to the count,-->
+          if ($scope.itemsArray[i].trim() != "") {
+            $scope.filteredItemsArray[j] = $scope.itemsArray[i].trim();
+            j = j + 1;
           }
         }
 
         if  ($scope.itemsArray.length === 1 && $scope.itemsArray[0] === "") {
-          $scope.text = "Please enter data first!!";
+          $scope.text = "Please enter data first";
           $scope.input_border = "red_border";
           $scope.text_color = "red_text";
-        } else if ($scope.itemsArray.length <= 3) {
+        } else if ($scope.filteredItemsArray.length <= 3) {
           $scope.text = "Enjoy!";
           $scope.input_border = "green_border";
           $scope.text_color = "green_text";
