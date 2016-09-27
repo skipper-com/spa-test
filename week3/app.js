b64c38dd8c5913ff80abe5e42dcebf750c240ab7
Alexander Pilugin <skipper-com@outlook.com>
2016-09-28 01:15:55 +0300

    week3_rep

--- week3/app.js
+++ week3/app.js
@@ -11,7 +11,7 @@ function FoundItemsDirective() {
   var ddo = {
     templateUrl: 'foundItems.html',
     scope: {
-      found: '<',
+      list: '<',
       myTitle: '@title',
       onRemove: '&'
     },
