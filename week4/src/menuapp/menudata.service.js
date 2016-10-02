3f98eb81b3820a26f0d40a6cd15555b0773d2dda
Alexander Pilugin <skipper-com@outlook.com>
2016-10-02 23:42:43 +0300

    week4_rep

--- week4/src/menuapp/menudata.service.js
+++ week4/src/menuapp/menudata.service.js
@@ -8,6 +8,7 @@ function MenuDataService() {
   var service = this;
 
   service.getAllCategories = function () {
+    console.log('getAllCategories');
     return $http({
       method: "GET",
       url: ("https://davids-restaurant.herokuapp.com/categories.json")
