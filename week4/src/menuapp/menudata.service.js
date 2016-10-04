4058372a31d2411b69556a479a74c7282294f096
Alexander Pilugin <skipper-com@outlook.com>
2016-10-05 00:12:05 +0300

    week4_rep

--- week4/src/menuapp/menudata.service.js
+++ week4/src/menuapp/menudata.service.js
@@ -25,6 +25,7 @@ function MenuDataService($http) {
   };
 
   service.getItemsForCategory = function (categoryShortName) {
+    console.log("short_name ", categoryShortName);
     return $http({
       method: "GET",
       url: ("https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName)
