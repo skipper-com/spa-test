04b239e50eedc15de5ba19a7192658bc2d5e8310
Alexander Pilugin <skipper-com@outlook.com>
2016-10-05 00:46:40 +0300

    week4_rep

--- week4/src/menuapp/items.component.js
+++ week4/src/menuapp/items.component.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('MenuApp')
-.component('items', {
+.component('categoriesDetails ', {
   templateUrl: 'src/menuapp/templates/items.template.html',
   bindings: {
     items: '<'
